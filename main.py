ad7c2200a3c014c237a0db7675da83baa875293f
Kodi Collins <kcollins@wifi-131-179-22-251.host.ucla.edu>
2019-05-22 10:47:56 -0700

    working

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from inout import IO
+from input import Input
 from methods import Methods
 import pandas                                                                   
 from scipy.stats import multivariate_normal                                     
 import numpy as np                                                              
 
-class Main(IO, Methods):
+class Main(Input, Methods):
   def __init__(self):
     super().__init__()
     self.percent = []
-    self.count = 0
     self.lrCrit = []
     self.lrValues = []
     self.lrPvalues = []
     self.lrMvalues = []
-    
+    self.lrSignif = []
     self.miCrit = []
     self.miValues = []
     self.miPvalues = []
     self.miMvalues = []
-    
-    self.signif = 0
+    self.miSignif = []
     self.null()
     self.analyze()
+    self.output()
 
   #null actually simulates the data
   def null(self):
@@ -56,17 +55,62 @@ class Main(IO, Methods):
   #analyze begins the actual analysis of the real input
   #TODO Needs to be generalized to different input.
   def analyze(self):
-    #self.combo = pandas.read_table(self.out)
+    lrLoc = []
+    miLoc = []
     zs = self.combo[self.combo.columns[self.combo.columns.to_series().str.contains('Z_')]]
     zs = np.array(zs)
-    print('zs: ' + str(zs))
     self.lrValues = self.lr_real(zs)
     self.lrPvalues =  self.pvalues(self.lrCrit, self.lrValues, self.percent)
-    #self.lrMvalues = Mvalues.method(zs[self.signif,:])
+    self.lrSignif = np.where(self.lrPvalues<= 5e-8)
+    self.lrSignif = np.squeeze(self.lrSignif, axis = 1)
+    loc = self.combo[['CHR','BP']]
+    #self.combo[self.combo.columns[self.combo.columns.to_series().str.contains("CHR|BP")]]
+    for i in range(1,23):
+      print('select: ' + str(i))
+      select = loc[loc['CHR'] == i]
+      print(select)
+      select = loc['BP']
+      print(select)
+      select = np.array(select)
+      print(select)
+      lrLoc = select[self.lrSignif]
+      print(lrLoc)
+    exit()
+    #print(loc)
+    #if self.alpha is -1.0:
+    #loc = self.combo[self.combo.columns[self.combo.columns.to_series().str.contains("CHR|BP")]]
+    #print('lrLoc')
+    #del loc
+    self.lrMvalues = self.mvalues(zs[self.lrSignif,:], lrLoc)
     if self.migwas:
       self.miValues = self.mi_real(zs)
       self.miPvalues = self.pvalues(self.miCrit, self.miValues, self.percent)
-      #self.miMvalues = Mvalues.method(zs[self.signif,:])
+      self.miSignif = np.where(self.miPvalues<= 5e-8)
+      self.miSignif = np.squeeze(self.miSignif, axis = 1)
+      #if self.alpha is -1.0:
+      #loc = np.array(loc)
+      print('miLoc')
+      miLoc = loc[self.miSignif,:]
+      #del loc
+      self.miMvalues = self.mvalues(zs[self.miSignif,:], miLoc)
+
+  #writes final output
+  #TODO: needs to be updated to work, combo not actually created yet
+  def output(self):
+    if self.migwas:
+      self.combo['MIGWAS_Score'] = self.miValues
+      self.combo['MIGWAS_Pvalue'] = self.miPvalues
+      for i in range(len(self.mean)):
+        x = np.zeros(int(self.count))
+        x[self.miSignif] = self.miMvalues[i]
+        self.combo['MIGWAS_Mvalue_'+self.traits[i]] = x
+    self.combo['PAT_Score'] = self.lrValues
+    self.combo['PAT_Pvalue'] = self.lrPvalues
+    for i in range(len(self.mean)):
+      x = np.zeros(int(self.count))
+      x[self.lrSignif] = self.lrMvalues[i]
+      self.combo['PAT_Mvalue_'+self.traits[i]] = x
+    self.combo.to_csv(self.out, sep=' ', index=False)
         
 if __name__=="__main__":
   Main()
--- methods.py
+++ methods.py
@@ -1,19 +1,10 @@
-from scipy.stats import multivariate_normal
+from scipy.stats import multivariate_normal, norm
 import math
 import numpy as np
+import itertools
 
 #This class contains the pat and migwas methods
 class Methods():
-  def __init__(self):
-    self.mean = []
-    self.sigmaE = []
-    self.sigmaG = []
-
-  #inheritance is weird
-  def set_input(self, mean, sigmaE, sigmaG):
-    self.mean = mean
-    self.sigmaE = sigmaE
-    self.sigmaG = sigmaG
 
   #pvalues maps the real data to critical values and returns the p-value
   def pvalues(self, crit, values, percent):
@@ -34,18 +25,111 @@ class Methods():
     crit = crit[keep]
     return crit
 
+  @staticmethod
+  def sumlog(x, y):
+    combine = np.stack((x,y), axis=1)
+    high = np.amax(combine, axis=1)
+    low = np.amin(combine, axis=1)
+    diff = np.subtract(high,low)
+    total = low + np.log1p(np.exp(diff))
+    return total
+  
+  def mvalues(self, data, loc):
+    k = data.shape[1]
+    alphaData = self.prune(data,loc)
+    include = [list(i) for i in list(itertools.product([1.0, 0.0], repeat=k))]
+    groups = [[] for i in range(k)]
+    mvalues = [[] for i in range(k)]
+    configs = []
+    #if self.alpha is -1.0:
+    print('set alpha')
+    self.set_alpha(alphaData)
+    #mat = np.cov(data,rowvar=False)
+    #mat = self.sigmaE + self.alpha*self.sigmaG
+    print('alpha: ' + str(self.alpha))
+    #print('sigmaE: ' + str(self.sigmaE))
+    #print('sigmaG: ' + str(self.sigmaG))
+    #print('mat: ' + str(mat))
+    #setting up alt config for mvalues and assigning index to group             
+    for z in range(0,len(include)):
+      alt = np.ones((k,k))
+      loc = include[z]
+      print('Loc: ' + str(loc))
+      for i in range(0,k):
+        for j in range(0,i+1):
+          if loc[i] == 0 or loc[j] == 0:
+            alt[i,j] = self.sigmaE[i,j]
+            alt[j,i] = self.sigmaE[j,i]
+          elif i == j:
+            alt[i,i] = self.sigmaE[i,i] + self.alpha[i]*self.sigmaG[i,i]
+          else:
+            corrG = self.sigmaG[i,j]/(math.sqrt(self.sigmaG[i,i])*math.sqrt(self.sigmaG[j,j]))
+            print('corrG: ' + str(corrG))
+            g = corrG*math.sqrt(self.alpha[i]*self.sigmaG[i,i])*math.sqrt(self.alpha[j]*self.sigmaG[j,j])
+            alt[i,j] = self.sigmaE[i,j] + g
+            alt[j,i] = self.sigmaE[j,i] + g
+      print('Alt: ' + str(alt))
+      configs.append(alt)
+      #assign to groups   
+      for l in range(0,k):
+        if loc[l] == 1.0:
+          groups[l].append(z)
+    all_configs = []
+    for i in range(0,len(configs)):
+      value = multivariate_normal.logpdf(data, self.mean, configs[i])
+      if len(all_configs) is 0:
+        all_configs = value
+      else:
+        all_configs = Methods.sumlog(all_configs, value)
+      for j in range(0,len(groups)):
+        if i in groups[j]:
+          if len(mvalues[j]) is 0:
+            mvalues[j] = value
+          else:
+            mvalues[j] = Methods.sumlog(mvalues[j], value)
+    mvalues = np.exp(np.subtract(mvalues,all_configs))
+    print('mvalues: ' + str(mvalues))
+    return mvalues
+
+  def prune(self, data, loc):
+    print('prune GWAS')
+    #print('loc: ' + str(loc))
+    for i in range(1,23):
+      select = loc[loc['CHR'] == i]
+      print(select)      
+
+      #t = t.rename(index=str, columns = {'SNP_'+traits[i]: 'SNP', 'CHR_'+traits[i]: 'CHR', 'BP_'+traits[i]: 'BP', 'A1_'+traits[i]: 'A1', 'A2_'+traits[i]: 'A2'})
+      #gwas = gwas.merge(t, how = 'inner', on=['SNP','CHR','BP','A1','A2'])
+    return data
+
+  def set_alpha(self, data):
+    k = data.shape[1]
+    maximum = [-np.inf for i in range(k)]
+    for i in range(100,100000,10):
+      covar = self.sigmaE + (float(i)/100.0)*self.sigmaG
+      for j in range(k):
+        #print(len(data[:,j]))
+        pdf = norm.logpdf(data[:,j], self.mean[j], covar[j,j])
+        total = np.sum(pdf)
+        #print('pdf: ' + str(pdf))
+        #print('total: ' + str(total))
+        if total > maximum[j]:
+          maximum[j] = total
+          self.alpha[j] = float(i)/100.0
+    #print('alpha: ' + str(self.alpha))
+
   #returns critical values for likelihood ratio method
   def lr_null(self, sims, weigh, percent):
-    null = multivariate_normal.logpdf(sims, self.mean, self.sigmaE)           
-    alt = multivariate_normal.logpdf(sims, self.mean, np.add(self.sigmaE,self.sigmaG))             
+    null = multivariate_normal.logpdf(sims, self.mean, self.sigmaE)
+    alt = multivariate_normal.logpdf(sims, self.mean, np.add(self.sigmaE,self.sigmaG))
     lrcrit = np.asarray(np.subtract(alt, null))
-    return self.process_crit(lrcrit, weigh, percent)    
+    return self.process_crit(lrcrit, weigh, percent)
 
   #returns lr results on real data
   def lr_real(self, data):
-    null = multivariate_normal.logpdf(data, self.mean, self.sigmaE)          
+    null = multivariate_normal.logpdf(data, self.mean, self.sigmaE)
     alt = multivariate_normal.logpdf(data, self.mean, np.add(self.sigmaE,self.sigmaG))
-    return np.asarray(np.subtract(alt, null)) 
+    return np.asarray(np.subtract(alt, null))
 
   #return critical values for migwas method
   def mi_null(self, sims, weigh, percent):
@@ -55,8 +139,3 @@ class Methods():
   #returns migwas results on real data
   def mi_real(self, data):
     return np.asarray(np.amax(abs(data), axis=1))
-
-  def mvalues(self, data):
-
-#if __name__=="__main__":
-#  Methods()
